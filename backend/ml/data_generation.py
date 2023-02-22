dd631f9de4e43d089ea9eb46c33839e39c9c84bd
dharithrirai <dharithrirai18@gmail.com>
2023-02-22 12:10:17 +0530

    Data_Generated

--- backend/ml/data_generation.py
+++ backend/ml/data_generation.py
@@ -1,19 +1,17 @@
 import cv2
 import numpy as np
-face_classifier = cv2.CascadeClassifier('C:/Users/User/AppData/Local/Programs/Python/Python38-32/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-
+face_classifier = cv2.CascadeClassifier('/home/dharithri/indecommSmartAttendance/backend/ml/haarcascade_frontalface_default.xml')
 def face_extractor(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
 
-    if faces is():
+    if faces == ():
         return None
 
     for(x,y,w,h) in faces:
         cropped_face = img[y:y+h, x:x+w]
-        
-    return cropped_face
 
+    return cropped_face
 
 cap = cv2.VideoCapture(0)
 count = 0
@@ -24,11 +22,8 @@ while True:
         count+=1
         face = cv2.resize(face_extractor(frame),(200,200))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-
-        file_name_path = 'C:/Users/User/Desktop/image/'+str(count)+'.jpg'
-
+        file_name_path = '/home/dharithri/indecommSmartAttendance/backend/ml/image/'+str(count)+'.jpg'
         cv2.imwrite(file_name_path,face)
-
         cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
         cv2.imshow('Face Cropper',face)
     else:
