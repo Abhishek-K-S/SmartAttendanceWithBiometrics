f429d8797428bdcb6658ce90580d04ae236aa719
ashay-sooda-24 <ashaysooda24@gmail.com>
2023-03-03 22:16:19 +0530

    Login Flow Basic implemented

--- frontend/indecommAttendance/App.js
+++ frontend/indecommAttendance/App.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import MainScreen from "./src/screens/MainScreen";
 import RegisterCameraScreen from "./src/screens/RegisterCameraScreen";
 import ViewFinalVideoScreen from "./src/screens/ViewFinalVideoScreen";
 
@@ -26,8 +28,21 @@ export default function App() {
           }}
         />
         <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerStyle: { backgroundColor: "#1E254D" },
+            headerTintColor: "white",
+          }}
+        />
+        <Stack.Screen
           name="ViewScreen"
           component={ViewFinalVideoScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
---
+++ frontend/indecommAttendance/src/screens/MainScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const MainScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>MainScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({})
