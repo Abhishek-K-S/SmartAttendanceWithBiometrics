41d6be2e932b457fa13afb0161dd9832ecc93c50
ashay-sooda-24 <ashaysooda24@gmail.com>
2023-03-05 14:19:17 +0530

    header added

--- frontend/indecommAttendance/src/components/MainScreen/HeaderMain.jsx
+++ frontend/indecommAttendance/src/components/MainScreen/HeaderMain.jsx
@@ -1,11 +1,18 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const HeaderMain = () => {
     return (
-        <View style={{ height: "100%", padding: "2%" }}>
+        <View style={styles.container}>
             <View style={styles.imgcontainer}>
                 <Image source={{ uri: "https://cdn.pixabay.com/photo/2018/08/26/23/55/woman-3633737__480.jpg" }} style={{ height: '100%', width: "100%", resizeMode: "cover" }} />
+            </View>
+            <View style={styles.logoContainer}>
+                <Image style={{ height: '100%', width: '100%', resizeMode: 'cover' }} source={require('../../../assets/imdecomVector.png')} />
+            </View>
+            <View style={styles.iconContainer}>
+                <Ionicons name='notifications' size={25} color="#1C2351" />
             </View>
         </View>
     )
@@ -14,11 +21,24 @@ const HeaderMain = () => {
 export default HeaderMain
 
 const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        padding: "2%",
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
     imgcontainer: {
-        height: " 90%",
-        width: "14%",
+        height: " 65%",
+        width: "10%",
         // borderWidth: 2,
         borderRadius: 30,
         overflow: 'hidden'
+    },
+    logoContainer: {
+        height: "90%",
+        width: "55%",
+        // borderWidth: 2,
+        overflow: 'hidden',
     }
 })
